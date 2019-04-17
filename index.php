c5babeeed7985d3c743ff08b648515e60b02facf
PSavlevic <you@example.comp.savlevic@gmail.com>
2019-04-17 16:11:26 +0300

    img

--- index.php
+++ index.php
@@ -1,28 +1,16 @@
 <?php
-$string = 'masina';
-$integer = 4;
-$float = 4.15;
-$boolean = true;
-$xuy = null;
+$img1 = 'http://www.clker.com/cliparts/7/Q/L/g/y/j/button-orange-small-md.png';
 ?>
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset="UTF-8">
     <title>uzduotis</title>
-    <style>
-        ul li {
-            list-style-type: none;
-        }
-    </style>
 </head>
 <body>
-    <ul>
-        <li><?php print "String: $string"; ?></li>
-        <li><?php print "Integer: $integer"; ?></li>
-        <li><?php print "Float: $float"; ?></li>
-        <li><?php print "Boolean: $boolean"; ?></li>
-        <li><?php print "XUY: $xuy"; ?></li>
-    </ul>
+    <img src="<?php print $img1; ?>" alt="1">
+    <img src="<?php print $img1; ?>" alt="1">
+    <img src="<?php print $img1; ?>" alt="1">
+    <img src="<?php print $img1; ?>" alt="1">
 </body>
 </html>
