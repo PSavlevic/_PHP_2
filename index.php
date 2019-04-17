991e4af01fa933694eeb0417dc46a0037f22b68c
PSavlevic <you@example.comp.savlevic@gmail.com>
2019-04-17 16:54:55 +0300

    3 uzduotis v4

--- index.php
+++ index.php
@@ -1,22 +1,22 @@
 <?php
 
-    $rnd1 = rand(1, 100);
-    $rnd2 = rand(101, 200);
-    $rnd3 = rand(201, 300);
+    $eur1 = rand(1, 100);
+    $eur2 = rand(101, 200);
+    $eur3 = rand(201, 300);
 
 ?>
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="UTF-8">
-    <title>uzduotis</title>
-</head>
-<body>
-    <div class="body1">
-        <h1>Skolos skaiciuokle</h1>
-        <h3>Jei paėmei <?php print $rnd11; ?>jievru</h3>
-        <h4>Su dviem kabanciais grazinsi <?php print $rnd3; ?></h4>
-        <h2>Su vienu kabanciu grazinsi <?php print $rnd2; ?></h2>
-    </div>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <title>uzduotis</title>
+    </head>
+    <body>
+        <div>
+            <h1>Skolos skaiciuokle</h1>
+            <h3>Jei paėmei <?php print $eur1; ?>jievru</h3>
+            <h4>Su dviem kabanciais grazinsi <?php print $eur3; ?></h4>
+            <h2>Su vienu kabanciu grazinsi <?php print $eur3; ?></h2>
+        </div>
+    </body>
 </html>
