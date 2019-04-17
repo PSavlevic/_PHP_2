9a3fb3474fb0ac04ac29b6e8b2d44b96dc9c6b1a
PSavlevic <you@example.comp.savlevic@gmail.com>
2019-04-17 17:06:06 +0300

    3 uzduotis v6

--- index.php
+++ index.php
@@ -12,11 +12,9 @@
         <title>uzduotis</title>
     </head>
     <body>
-        <div>
-            <h1>Skolos skaiciuokle</h1>
-            <h3>Jei paėmei <?php print $eur1; ?> euru.</h3>
-            <h4>Su dviem kabanciais grazinsi <?php print $eur3; ?> euru.</h4>
-            <h2>Su vienu kabanciu grazinsi <?php print $eur3; ?> euru.</h2>
-        </div>
+        <h1>Skolos skaiciuokle</h1>
+        <h3>Jei paėmei <?php print $eur1; ?> euru.</h3>
+        <h4>Su dviem kabanciais grazinsi <?php print $eur3; ?> euru.</h4>
+        <h2>Su vienu kabanciu grazinsi <?php print $eur3; ?> euru.</h2>
     </body>
 </html>
