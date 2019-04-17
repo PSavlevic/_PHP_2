49ab6852a8b35e47c798153c53c57a03bc803176
PSavlevic <you@example.comp.savlevic@gmail.com>
2019-04-17 16:22:15 +0300

    img repaired

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
+
 $img1 = 'http://www.clker.com/cliparts/7/Q/L/g/y/j/button-orange-small-md.png';
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -8,9 +10,9 @@ $img1 = 'http://www.clker.com/cliparts/7/Q/L/g/y/j/button-orange-small-md.png';
     <title>uzduotis</title>
 </head>
 <body>
-    <img src="<?php print $img1; ?>" alt="1">
-    <img src="<?php print $img1; ?>" alt="1">
-    <img src="<?php print $img1; ?>" alt="1">
-    <img src="<?php print $img1; ?>" alt="1">
+    <img src="<?php print $img1; ?>">
+    <img src="<?php print $img1; ?>">
+    <img src="<?php print $img1; ?>">
+    <img src="<?php print $img1; ?>">
 </body>
 </html>
