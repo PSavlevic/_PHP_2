b3f4a8fd59d8583a69aaba2e9e6446bc2b89780d
PSavlevic <you@example.comp.savlevic@gmail.com>
2019-04-17 14:53:32 +0300

    variables

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
-<?php
-print 'testass'
-?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>uzduotis</title>
+</head>
+<body>
+    <?php
+        $string = 'masina';
+        $integer = 4;
+        $float = 4.15;
+        $boolean = true;
+        $xuy = null;
+
+        print "String: $string <br>";
+        print "String: $integer <br>";
+        print "String: $float <br>";
+        print "String: $boolean <br>";
+        print "String: $xuy <br>";
+    ?>
+
+</body>
+</html>
