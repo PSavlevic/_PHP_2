18d4a81e2f946ce90ae5670a6cc589dda7f9c4f8
PSavlevic <you@example.comp.savlevic@gmail.com>
2019-04-17 15:39:01 +0300

    variables finally

--- index.php
+++ index.php
@@ -1,23 +1,28 @@
+<?php
+$string = 'masina';
+$integer = 4;
+$float = 4.15;
+$boolean = true;
+$xuy = null;
+?>
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset="UTF-8">
     <title>uzduotis</title>
+    <style>
+        ul li {
+            list-style-type: none;
+        }
+    </style>
 </head>
 <body>
-    <?php
-        $string = 'masina';
-        $integer = 4;
-        $float = 4.15;
-        $boolean = true;
-        $xuy = null;
-
-        print "String: $string <br>";
-        print "Integer: $integer <br>";
-        print "Float: $float <br>";
-        print "Boolean: $boolean <br>";
-        print "XUY: $xuy <br>";
-    ?>
-
+    <ul>
+        <li><?php print "String: $string"; ?></li>
+        <li><?php print "Integer: $integer"; ?></li>
+        <li><?php print "Float: $float"; ?></li>
+        <li><?php print "Boolean: $boolean"; ?></li>
+        <li><?php print "XUY: $xuy"; ?></li>
+    </ul>
 </body>
 </html>
