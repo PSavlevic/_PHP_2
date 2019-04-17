cfff623964633f59c689cb5500f90580b0489acf
PSavlevic <you@example.comp.savlevic@gmail.com>
2019-04-17 16:51:12 +0300

    3 uzduotis v3

--- index.php
+++ index.php
@@ -14,9 +14,9 @@
 <body>
     <div class="body1">
         <h1>Skolos skaiciuokle</h1>
-        <h3>Jei paėmei <?php print $sudas1; ?>jievru</h3>
-        <h4>Su dviem kabanciais grazinsi <?php print $sudas3; ?></h4>
-        <h2>Su vienu kabanciu grazinsi <?php print $sudas2; ?></h2>
+        <h3>Jei paėmei <?php print $rnd11; ?>jievru</h3>
+        <h4>Su dviem kabanciais grazinsi <?php print $rnd3; ?></h4>
+        <h2>Su vienu kabanciu grazinsi <?php print $rnd2; ?></h2>
     </div>
 </body>
 </html>
