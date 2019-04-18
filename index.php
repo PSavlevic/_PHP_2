c905b366fe649014b46e6fb25bbc1dd06df29ad8
PSavlevic <you@example.comp.savlevic@gmail.com>
2019-04-18 11:49:17 +0300

    10 uzd (bomba)

--- index.php
+++ index.php
@@ -1,22 +1,43 @@
 <?php
-$turis = 40;
-$avg = 15;
-$max = rand(10, 40);
-$dienu =  floor(($turis + $max) / $avg);
+
+$time = date('s');
+$timer = $time % 60;
+$explo = $timer<1;
+
+$plius = date('s') + 50;
+
+$img_height = $plius + 150 . 'px;';
+$img_width = $plius + 150 . 'px;';
 
 ?>
+
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title>uzduotis</title>
-    </head>
-    <body>
-        <h1>Po keliu dienu kils barnis?</h1>
-        <ul>
-            <li>
-            Po <?php print $dienu . ' dienu' . ' (' . date('Y-m-d', strtotime("+$dienu days")) . ')' ?> pirk geliu ir sampano, jeigu nori, kad zmona siuksles pati isnestu.
-            </li>
-        </ul>
-    </body>
+<head>
+    <title></title>
+    <style type="text/css">
+        .imgboom {
+            background-image: url('https://www.gamemodd.com/uploads/posts/2017-05/1496003312_cs-go-c4.jpg?fbclid=IwAR0lqUFsuwBqgsnUFFHvs2x7Y8ER1wsLyTRntrNH_WROaC6WJg5e1_6I7Rg');
+            background-size: cover;
+            height: <?php print $img_height; ?>
+            width: <?php print $img_width; ?>
+        }
+        .imgboom-1 {
+            background-image: url('https://3c1703fe8d.site.internapcdn.net/newman/csz/news/800/2017/1-fiveofthemos.jpg?fbclid=IwAR16k40XeALTn3GFJ-KQ9vor2vWdzaigCPJW3PgSN4HsHWQXGeJGx3ojUPg');
+            background-size: cover;
+            height: 150px;
+            width: 150px;
+        }
+    </style>
+</head>
+<body>
+<div class="imgboom-<?php print $explo; ?> imgboom"></div>
+<?php
+
+print $time . '<br>';
+print $timer . '<br>';
+print $explo . '<br>';
+
+?>
+</body>
 </html>
