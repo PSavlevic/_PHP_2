b5692d9e12673e7cd081bd54cf3221c1ba22f395
PSavlevic <you@example.comp.savlevic@gmail.com>
2019-04-19 10:35:25 +0300

    5 uzd taisyklingai su Font Awesome

--- index.php
+++ index.php
@@ -1,26 +1,34 @@
 <?php
+$sunny = rand(0, 1);
+$rainy = rand(0, 1);
 
-$x = rand(0, 1);
-
-if ($x) {
-    $css_class = 'sauleta';
-    $text1 = 'Sauleta';
-} else {
-    $css_class = 'lietus';
-    $text1 = 'Lietus';
+if ($sunny && $rainy) {
+    $css_class = 'fas fa-rainbow';
+    $text = "Rainbow";
+} elseif ($sunny && !$rainy) {
+    $css_class = 'sunny';
+    $text = 'Sunny';
+} elseif (!$sunny && $rainy) {
+    $css_class = 'rain';
+    $text = 'Rain';
+} elseif (!$sunny && !$rainy) {
+    $css_class = 'fas fa-cloud-sun';
+    $text = 'Debesuota';
 }
 ?>
 <!DOCTYPE html>
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>title</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
+    <title>title</title>
+
 </head>
 <body>
 <div class="container">
-    <h1 class="<?php print $css_class; ?>">
-        <?php print "$text1";?>
+    <h1 class="<?php print "$css_class"; ?>">
+        <?php print $text; ?>
     </h1>
 </div>
 </body>
